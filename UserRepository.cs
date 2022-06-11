046fbd07f01c72a600876609f7495ed51724c9c1
Chu Xuân Tình <50776038+chuxuantinh@users.noreply.github.com>
2022-06-11 13:25:24 +0700

    Initial commit

---
+++ UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using JWTASPNetCore.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JWTASPNetCore
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly List<UserDTO> users = new List<UserDTO>();
+        public UserRepository()
+        {
+            users.Add(new UserDTO { UserName = "chutinh", Password = "123", Role = "manager" });
+            users.Add(new UserDTO { UserName = "xuan", Password = "123", Role = "developer" });
+            users.Add(new UserDTO { UserName = "tinh", Password = "123", Role = "admin" });
+        }
+        public UserDTO GetUser(UserModel userModel)
+        {
+            return users.Where(x => x.UserName.ToLower() == userModel.UserName.ToLower()
+                && x.Password == userModel.Password).FirstOrDefault();
+        }
+    }
+}
